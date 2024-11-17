aaaa885bcc020bd519653c0f4d0b52e96c387c62
Lucien <2531989856@qq.com>
2024-11-17 15:16:20 +0800

    Added fit linearity judgment step.

--- Calculate_Fractal_Dimension.m
+++ Calculate_Fractal_Dimension.m
@@ -11,7 +11,7 @@ Max_Dis = max(Dis);
 Factor = Max_Dis/(10*Min_Dis);
 
 %Setting the box size sequence
-box_sizes = logspace(log10(1/20),0,20).*(Factor*Min_Dis);
+box_sizes = logspace(log10(1/10),0,20).*(Factor*Min_Dis);
 
 %Initialize arrays to store the results
 N_boxes = zeros(size(box_sizes));
@@ -25,18 +25,22 @@ for i = 1:length(box_sizes)
 end
 
 % Calculate the slope of the log-log plot to estimate the fractal dimension
-p = polyfit(log_box_sizes, log_N_boxes, 1);
-Fractal_Dimension = -p(1);
+[p,gof] = fit(log_box_sizes', log_N_boxes','poly1');
+Fractal_Dimension = -p.p1;
 
 % Display the fractal dimension
-disp(['The estimated fractal dimension of the trajectory is: ', num2str(Fractal_Dimension),newline]);
+if gof.adjrsquare >= 0.95
+    disp(['The estimated fractal dimension of the trajectory is: ', num2str(Fractal_Dimension),newline]);
+else
+    warning('Poor linearity of results, please re-select box size range !')
+end
 
 % Visualization
 figure;
 set(gcf,'Position',[20 20 1200 1000])
 plot(log_box_sizes, log_N_boxes, 'bo','MarkerSize',7.5,'LineWidth',1.0);
 hold on;
-plot(log_box_sizes, polyval(p, log_box_sizes), 'r-','LineWidth',1.5);
+plot(log_box_sizes, p(log_box_sizes), 'r-','LineWidth',1.5);
 set(gca,'FontName','Times New Roman')
 grid on;
 ax = gca; ax.LineWidth = 1.5;
@@ -46,7 +50,7 @@ ylabel('log(NumBox)','FontSize',18);
 title('Fractal Dimension Calculation','FontSize',24,'FontWeight','bold');
 legend({'Original Data','Fitted Line'},'FontSize',16,'Location','northeast')
 latexf = ['$$ \leftarrow {\bf FD} = ',num2str(Fractal_Dimension),'$$'];
-text(log_box_sizes(floor(i/2)),polyval(p, log_box_sizes(floor(i/2))),latexf,'Interpreter','latex','FontSize',14, ...
+text(log_box_sizes(floor(i/2)),p(log_box_sizes(floor(i/2))),latexf,'Interpreter','latex','FontSize',14, ...
     'Color','k','HorizontalAlignment','left')
 hold off;
 
